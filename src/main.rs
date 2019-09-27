06ea6a65efcc991e667ecf54c525540dcccb4929
timvisee <tim@visee.me>
2019-09-27 17:39:43 +0200

    Render to input files, implement scene file change watcher

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[macro_use]
+extern crate clap;
+#[macro_use]
 extern crate derive_builder;
 #[macro_use]
 extern crate lazy_static;
@@ -7,8 +9,14 @@ extern crate serde_derive;
 
 use std::f32::consts::PI;
 use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::process;
+use std::sync::mpsc::channel;
+use std::time::Duration;
 
+use clap::{App, Arg};
 use image::{DynamicImage, GenericImage, Rgba};
+use notify::{RecursiveMode, Watcher};
 use rayon::prelude::*;
 use serde_yaml;
 
@@ -26,16 +34,138 @@ use math::*;
 use scene::*;
 
 fn main() {
+    // CLI argument handling
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("SCENE")
+                .help("Scene file to render")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Image file to output render to")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("watch")
+                .long("watch")
+                .short("w")
+                .help("Watch scene file, automatically rerender")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("open")
+                .long("open")
+                .short("o")
+                .help("Open rendered scene image")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    // Validate scene file
+    let scene_path = PathBuf::from(matches.value_of("SCENE").unwrap());
+    if !scene_path.is_file() {
+        eprintln!(
+            "Invalid scene file, not an existing file: '{}'",
+            scene_path.to_str().unwrap_or("?"),
+        );
+        process::exit(1);
+    }
+
+    // Validate render output file
+    let output_path = PathBuf::from(matches.value_of("OUTPUT").unwrap());
+    if output_path.is_dir() {
+        eprintln!(
+            "Invalid output file, is an existing directory: '{}'",
+            output_path.to_str().unwrap_or("?"),
+        );
+        process::exit(1);
+    }
+
+    // Check whether to open and watch
+    let mut open = matches.is_present("open");
+    let watch = matches.is_present("watch");
+
+    // Do not watch, render a single time and quit
+    if !watch {
+        run(open, scene_path.as_path(), output_path.as_path());
+        return;
+    }
+
+    // Create scene file watcher, with channel to receive events
+    let (tx, rx) = channel();
+    let mut watcher = notify::raw_watcher(tx).expect("failed to create file watcher");
+    watcher
+        .watch(&scene_path, RecursiveMode::NonRecursive)
+        .expect("failed to configure watcher for scene file changes");
+
+    // Keep rendering for each scene file change
+    loop {
+        // Render the scene
+        run(open, &scene_path, &output_path);
+
+        // Wait for scene file change
+        let event = rx.recv().expect("failed to watch scene file for changes");
+        eprintln!("EVENT: {:?}", event);
+
+        // Do not open a second time
+        open = false;
+
+        eprintln!("");
+    }
+}
+
+fn run(open: bool, scene_path: &Path, output_path: &Path) {
     // Load scene from file
-    eprintln!("Loading scene from file...");
-    let scene_file = File::open("scenes/default.yml").expect("failed to open scene file");
-    let scene = serde_yaml::from_reader(scene_file).expect("failed to parse scene file");
-    eprintln!("Scene loaded");
+    eprintln!("Loading scene file...");
+    let scene_file = match File::open(scene_path) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!(
+                "Failed to open scene file, could not open file at '{}'\nSkipping this render\n\nDetails:\n{}",
+                scene_path.to_str().unwrap_or("?"),
+                err,
+            );
+            return;
+        }
+    };
+    let scene = match serde_yaml::from_reader(scene_file) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!(
+                "Failed to parse scene file, skipping this render\n\nDetails:\n{}",
+                err,
+            );
+            return;
+        }
+    };
 
     // Render scene to an image, save it to a file
-    render(&scene)
-        .save("render.png")
-        .expect("failed to save render to image file");
+    eprintln!("Rendering scene...");
+    let render = render(&scene);
+    match render.save(output_path) {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!(
+                "Failed to write render to output path, could not write at: '{}'\nSkipping this render\n\nDetails:\n{}",
+                output_path.to_str().unwrap_or("?"),
+                err,
+            );
+            return;
+        }
+    }
+    eprintln!("Rendering finished");
+
+    // Open render file
+    if open {
+        eprintln!("Opening render file...");
+        open::that(output_path).expect("failed to open render output file");
+    }
 }
 
 /// Render the given scene.
