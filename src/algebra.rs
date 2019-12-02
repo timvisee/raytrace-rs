2a6b9f952411f022b80d18d9ce5674f94f0a964b
timvisee <tim@visee.me>
2019-12-02 17:17:35 +0100

    Fix cross product calculation, add basic unit tests for it

--- src/algebra.rs
+++ src/algebra.rs
@@ -26,7 +26,7 @@ impl Vector {
     pub fn cross(self, other: Self) -> Self {
         Self(
             self.1 * other.2 - self.2 * other.1,
-            self.0 * other.2 - self.2 * other.0,
+            self.2 * other.0 - self.0 * other.2,
             self.0 * other.1 - self.1 * other.0,
         )
     }
@@ -40,7 +40,7 @@ impl Vector {
     /// Squared magnitude or length.
     #[inline]
     pub fn magnitude_squared(self) -> Unit {
-        self.0.powi(2) + self.1.powi(2) + self.2.powi(2)
+        self.0 * self.0 + self.1 * self.1 + self.2 * self.2
     }
 }
 
@@ -115,5 +115,70 @@ pub trait Identity {
 impl Identity for Vector {
     fn identity() -> Self {
         Vector(0.0, 0.0, 0.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::f64::EPSILON;
+
+    #[test]
+    fn test_dot() {
+        assert_unit_equal(Vector(0.0, 0.0, 0.0).dot(Vector(0.0, 0.0, 0.0)), 0.0);
+        assert_unit_equal(Vector(1.0, 1.0, 1.0).dot(Vector(1.0, 1.0, 1.0)), 3.0);
+        assert_unit_equal(Vector(1.0, 2.0, 3.0).dot(Vector(3.0, 4.0, 5.0)), 26.0);
+        assert_unit_equal(Vector(1.0, 2.0, 3.0).dot(Vector(-1.0, -2.0, -3.0)), -14.0);
+        assert_unit_equal(
+            Vector(2.0, 9.5, 0.0).dot(Vector(14.2, 12.0, 36.0)),
+            712.0 / 5.0,
+        );
+    }
+
+    #[test]
+    fn test_cross() {
+        assert_vector_equal(
+            Vector(0.0, 0.0, 0.0).cross(Vector(0.0, 0.0, 0.0)),
+            Vector(0.0, 0.0, 0.0),
+        );
+        assert_vector_equal(
+            Vector(1.0, 1.0, 1.0).cross(Vector(1.0, 1.0, 1.0)),
+            Vector(0.0, 0.0, 0.0),
+        );
+        assert_vector_equal(
+            Vector(1.0, 2.0, 3.0).cross(Vector(3.0, 4.0, 5.0)),
+            Vector(-2.0, 4.0, -2.0),
+        );
+        assert_vector_equal(
+            Vector(1.0, 2.0, 3.0).cross(Vector(-1.0, -2.0, -3.0)),
+            Vector(0.0, 0.0, 0.0),
+        );
+        assert_vector_equal(
+            Vector(2.0, 9.5, 0.0).cross(Vector(14.2, 12.0, 36.0)),
+            Vector(342.0, -72.0, -110.9),
+        );
+    }
+
+    /// Check whether units are almost equal, taking the epsilon into account.
+    fn assert_unit_equal(a: Unit, b: Unit) {
+        assert!(
+            (a - b).abs() < EPSILON,
+            "floats {} and {} are not almost equal",
+            a,
+            b
+        );
+    }
+
+    /// Check whether units are almost equal, taking the epsilon into account.
+    fn assert_vector_equal(a: Vector, b: Vector) {
+        assert!(
+            (a.0 - b.0).abs() < EPSILON
+                && (a.1 - b.1).abs() < EPSILON
+                && (a.2 - b.2).abs() < EPSILON,
+            "vectors {:?} and {:?} are not almost equal",
+            a,
+            b
+        );
     }
 }
